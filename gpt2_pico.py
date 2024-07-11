e8512733bd66d11465c2d5e153853d47058b10f0
Ameer Arsala <56413920+AmeerArsala@users.noreply.github.com>
2024-07-11 13:43:35 -0700

    RANified gpt2_pico.py

--- gpt2_pico.py
+++ gpt2_pico.py
@@ -1,26 +1,35 @@
+import ran
+
 import numpy as np
 
+@ran.expose
 def gelu(x):
     return 0.5 * x * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * x**3)))
 
+@ran.expose
 def softmax(x):
     exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
     return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
 
+@ran.expose
 def layer_norm(x, g, b, eps: float = 1e-5):
     mean = np.mean(x, axis=-1, keepdims=True)
     variance = np.var(x, axis=-1, keepdims=True)
     return g * (x - mean) / np.sqrt(variance + eps) + b
 
+@ran.expose
 def linear(x, w, b):
     return x @ w + b
 
+@ran.expose
 def ffn(x, c_fc, c_proj):
     return linear(gelu(linear(x, **c_fc)), **c_proj)
 
+@ran.expose
 def attention(q, k, v, mask):
     return softmax(q @ k.T / np.sqrt(q.shape[-1]) + mask) @ v
 
+@ran.expose
 def mha(x, c_attn, c_proj, n_head):
     x = linear(x, **c_attn)
     qkv_heads = list(map(lambda x: np.split(x, n_head, axis=-1), np.split(x, 3, axis=-1)))
@@ -29,25 +38,29 @@ def mha(x, c_attn, c_proj, n_head):
     x = linear(np.hstack(out_heads), **c_proj)
     return x
 
+@ran.expose
 def transformer_block(x, mlp, attn, ln_1, ln_2, n_head):
     x = x + mha(layer_norm(x, **ln_1), **attn, n_head=n_head)
     x = x + ffn(layer_norm(x, **ln_2), **mlp)
     return x
 
+@ran.expose
 def gpt2(inputs, wte, wpe, blocks, ln_f, n_head):
     x = wte[inputs] + wpe[range(len(inputs))]
     for block in blocks:
         x = transformer_block(x, **block, n_head=n_head)
     return layer_norm(x, **ln_f) @ wte.T
 
+@ran.expose
 def generate(inputs, params, n_head, n_tokens_to_generate):
-    from tqdm import tqdm
-    for _ in tqdm(range(n_tokens_to_generate), "generating"):
+    #from tqdm import tqdm
+    for _ in range(n_tokens_to_generate):
         logits = gpt2(inputs, **params, n_head=n_head)
         next_id = np.argmax(logits[-1])
         inputs.append(int(next_id))
     return inputs[len(inputs) - n_tokens_to_generate :]
 
+@ran.expose
 def main(prompt: str, n_tokens_to_generate: int = 40, model_size: str = "124M", models_dir: str = "models"):
     from utils import load_encoder_hparams_and_params
     encoder, hparams, params = load_encoder_hparams_and_params(model_size, models_dir)
@@ -57,6 +70,6 @@ def main(prompt: str, n_tokens_to_generate: int = 40, model_size: str = "124M", 
     output_text = encoder.decode(output_ids)
     return output_text
 
-if __name__ == "__main__":
-    import fire
-    fire.Fire(main)
+#if __name__ == "__main__":
+#    import fire
+#    fire.Fire(main)
